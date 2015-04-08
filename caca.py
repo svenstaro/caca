0645b728be0d5f70d2dd1e164a481be7bb4209d9
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-04-08 23:53:13 +0200

    A bit of progress

--- caca.py
+++ caca.py
@@ -3,10 +3,11 @@
 
 import argparse
 import shutil
+import os
 
 DESCRIPTION = """caca - Copy And Convert Audio.
 
-A very simple one-shot parallel audio converter that behaves a lot cp"""
+A very simple one-shot parallel audio converter that behaves a lot like cp"""
 
 
 def detect_utils():
@@ -16,8 +17,20 @@ def detect_utils():
             raise FileNotFoundError("'{}' was not found on your system but is required".format(tool))
 
 
-def handle_file(src):
-    
+def handle_path(src, target):
+    for entry in os.walk(src):
+        for file in entry[2]:
+            if os.path.splitext(file)[1].tolower() == ".flac":
+                #os.makedirs(
+                # encoding magic
+                if args.verbose:
+                    print("'{}' -> '{}'".format(src, target))
+            else:
+                dst = target
+                #os.makedirs(dst, exist_ok=True)
+                if args.verbose:
+                    print("'{}' -> '{}'".format(src, target))
+
 
 def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
@@ -30,8 +43,16 @@ def main():
 
     detect_utils()
 
-    if args.verbose:
-        print("Copying {} to {}")
+    # Handle case where we have more than one sources but target is not a directory
+    if len(args.sources) > 1 and os.path.isfile(args.target):
+        raise RuntimeException("target '{}' is not a directory".format(args.target))
+
+    # Handle case where the source is a directory but target is a file
+    if os.path.isdir(args.sources[0]) and os.path.isfile(args.target):
+        raise RuntimeException("cannot overwrite non-directory '{}' with directory '{}'".format(args.target, args.sources[0]))
+
+    for src in args.sources:
+        handle_path(src)
 
 if __name__ == '__main__':
     main()
